ef55c86618aeaa2e60ce4fb4b8d05e81f4ebe5e6
Jmeas <jellyes2@gmail.com>
2014-11-29 14:10:24 -0500

    Return users to the same page upon logging in.
    Resolves #30

--- client.src/modules/home/home-route.js
+++ client.src/modules/home/home-route.js
@@ -9,6 +9,17 @@ import Gist from '../../shared/entities/gist';
 import GistView from '../../shared/views/gist-view';
 
 export default Route.extend({
+
+  // We check to see if the user has a cached uri fragment in session storage.
+  // If they do, then that means they logged in from some other page. If that's the case,
+  // then we want to redirect them to that page.
+  redirect: function() {
+    var cachedUriFragment = sessionStorage.getItem('cachedFragment');
+    if (!cachedUriFragment) { return; }
+    sessionStorage.removeItem('cachedFragment');
+    return cachedUriFragment;
+  },
+
   show: function() {
     var gistView = new GistView({
       model: new Gist(),
