59c45a607f973db692caa95669d58795e0c90b13
Jmeas <jellyes2@gmail.com>
2014-11-29 14:10:24 -0500

    Return users to the same page upon logging in.
    Resolves #30

--- client.src/core/views/menu-views/unauth-menu-view/index.js
+++ client.src/core/views/menu-views/unauth-menu-view/index.js
@@ -4,8 +4,18 @@
 // header
 //
 
+import * as bb from 'backbone';
 import * as mn from 'marionette';
 
 export default mn.ItemView.extend({
-  template: 'loginView'
+  template: 'loginView',
+
+  events: {
+    'click a': 'onClickLogin'
+  },
+
+  // Save the current page in sessionStorage
+  onClickLogin: function() {
+    sessionStorage.setItem('cachedFragment', bb.history.fragment);
+  }
 });
