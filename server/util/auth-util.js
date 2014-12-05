b4da9f12135986dcce3660677999b975adb7bad7
Jmeas <jellyes2@gmail.com>
2014-12-04 19:36:24 -0500

    Remove client-config file from local development server.
    Also moves the client-config file to the general app config folder.
    Resolves #278

--- server/util/auth-util.js
+++ server/util/auth-util.js
@@ -6,10 +6,10 @@
 
 var ENV = process.env.NODE_ENV || 'dev';
 var github = require('octonode');
-var clientInfo = require('../config/client-info.json')[ENV];
 
 var authUtil = {
   url: function() {
+    var clientInfo = require('../../config/client-info.json')[ENV];
     return github.auth.config({
       id: clientInfo.clientId,
       secret: clientInfo.clientSecret
