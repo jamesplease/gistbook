6e5575e38a0018bdf17bef151da40a2b934539de
Jmeas <jellyes2@gmail.com>
2014-12-07 01:17:38 -0500

    Add new events to the Router.

--- client.src/vendor/routing/router.js
+++ client.src/vendor/routing/router.js
@@ -16,30 +16,35 @@ export default BaseRouter.extend({
     var redirect = _.result(newRoute, 'redirect');
     if (_.isString(redirect)) {
       this.navigate(redirect, {trigger:true});
+      newRoute.triggerMethod('redirect', routeData);
       return;
     }
 
     if (newRoute.authorize && !Radio.request('auth', 'authorized')) {
       this.navigate('login', {trigger:true});
+      newRoute.triggerMethod('unauthorized', routeData);
       return;
     }
 
     if (this.currentRoute) {
-      this.currentRoute.triggerMethod('exit');
+      this.currentRoute.triggerMethod('exit', routeData);
     }
     this.currentRoute = newRoute;
-    newRoute.triggerMethod('enter');
+    newRoute.triggerMethod('enter', routeData);
 
     if (!newRoute.fetch) {
       newRoute.show(undefined, routeData);
+      newRoute.triggerMethod('show', routeData);
     } else {
 
       // Wait for the data to come back, then
       // show the view if the route is still active.
       Promise.resolve(newRoute.fetch(routeData))
         .then(data => {
+          newRoute.triggerMethod('fetch', routeData, data);
           if (newRoute !== this.currentRoute) { return; }
           newRoute.show(data, routeData);
+          newRoute.triggerMethod('show', routeData);
         })
         .catch(e => {
           if (newRoute !== this.currentRoute) { return; }
