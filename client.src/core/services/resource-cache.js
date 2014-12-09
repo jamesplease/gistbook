e5b195ab400b0593bb68dd691b093384ca8f090b
Jmeas <jellyes2@gmail.com>
2014-12-08 22:53:51 -0500

    Update resource cache after all AJAX requests.
    Resolves #334

---
+++ client.src/core/services/resource-cache.js
@@ -0,0 +1,30 @@
+//
+// Resource Cache
+// Caches API resources by URI
+//
+
+import * as _ from 'underscore';
+import * as $ from 'jquery';
+
+var cache = {};
+var cacheTypes = ['PUT', 'GET', 'POST', 'PATCH'];
+
+$(document).ajaxSuccess(function(event, xhr, settings) {
+  var baseUrl = settings.url.split('?')[0];
+
+  // Do nothing if the request type isn't one that we cache
+  if (!_.contains(cacheTypes, settings.type)) { return; }
+
+  // Upon success, we save the response to the cache
+  xhr.then(function(resp, textStatus, jqXHR) {
+    if (textStatus !== 'notmodified') {
+      cache[baseUrl] = resp;
+    }
+  });
+});
+
+export default {
+  get(uri) {
+    return cache[uri] || undefined;
+  }
+};
