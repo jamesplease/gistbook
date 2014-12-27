42ec1cdc4e45b151532f1b2dfa994172f7b94b54
Jmeas <jellyes2@gmail.com>
2014-12-26 19:22:39 -0500

    Display time remaining on rate limit view; refresh when 0.
    Update Underscore dependency to 1.7.0

--- client.src/shared/views/error-rate-limit-view/index.js
+++ client.src/shared/views/error-rate-limit-view/index.js
@@ -4,6 +4,7 @@
 // see this view
 //
 
+import * as _ from 'underscore';
 import * as Radio from 'radio';
 import * as mn from 'marionette';
 
@@ -11,12 +12,63 @@ export default mn.ItemView.extend({
   initialize: function(options) {
     mn.mergeOptions(this, options, this.errorRateLimitOptions);
   },
+
   errorRateLimitOptions: ['resetTimestamp'],
+
   template: 'errorRateLimitView',
+  
   className: 'error-rate-limit',
+
+  ui: {
+    $minutesLeft: '.minutes-left',
+    $secondsLeft: '.seconds-left'
+  },
+  
   templateHelpers: function() {
+    this.now = Math.floor(Date.now() / 1000);
+    var diff = this.resetTimestamp - this.now;
     return {
+      minutesLeft: this._minutesLeft(diff),
+      secondsLeft: this._secondsLeft(diff),
       loggedIn: Radio.request('auth', 'authorized')
     };
+  },
+
+  // The following two methods determine the # of
+  // minutes & seconds in a unix timestamp. In this
+  // case, the timestamp is a difference, and what we
+  // get is how much longer you need to wait
+  _minutesLeft: function(diff) {
+    return Math.floor(diff / 60);
+  },
+
+  _secondsLeft: function(diff) {
+    return diff % 60;
+  },
+
+  updateTime: function(diff) {
+    this.ui.$minutesLeft.text(this._minutesLeft(diff));
+    this.ui.$secondsLeft.text(this._secondsLeft(diff));
+  },
+
+  scheduleUpdate: function(diff) {
+    diff--;
+    this.updateTime(diff);
+    if (diff) {
+      this.scheduleUpdate(diff);
+    } else {
+      this.reload();
+    }
+  },
+
+  reload: function() {
+    window.document.location.reload();
+  },
+  
+  // Ensure that scheduleUpdate is only called once per second,
+  // then call it
+  onRender: function() {
+    this.scheduleUpdate = _.debounce(this.scheduleUpdate, 1000);
+    this.scheduleUpdate(this.resetTimestamp - this.now);
   }
 });
